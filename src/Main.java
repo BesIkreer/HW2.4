8760ffe8b1178a894af5c3ef70c45a513052dcf4
besik <besik.reer@gmail.com>
2023-04-25 20:00:48 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        boolean result = Validator.validate("Tekst_54321", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
+        boolean result = Validator.validate("Tekst_543211", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
         if (result) {
             System.out.println("Логиин и пароль корректны.");
         }
